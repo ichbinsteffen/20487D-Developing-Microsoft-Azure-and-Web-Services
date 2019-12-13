743c625c85645a51046954f50ae5b621a65c5486
Steffen Ebner <Ichbinsteffen@gmail.com>
2019-12-13 09:26:53 +0100

    Added new webapi template

---
+++ AllFiles/Mod08/DemoFiles/Mod8Demo1Logger/Mod8Demo1LoggerStarter/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Mod8Demo1LoggerStarter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) => 
+                {
+                    logging.AddConsole();
+                    logging.AddEventSourceLogger();
+                });
+    }
+}
